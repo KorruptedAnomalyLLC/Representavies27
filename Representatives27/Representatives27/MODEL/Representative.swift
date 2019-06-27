6cd4a28568d0e10a3f724c426ea60d7004a0f106
Austin West <korruptedanomaly@gmail.com>
2019-06-27 16:07:22 -0600

    Storyboard set up, model controller done

---
+++ Representatives27/Representatives27/MODEL/Representative.swift
@@ -0,0 +1,21 @@
+//
+//  Representative.swift
+//  Representatives27
+//
+//  Created by Austin West on 6/27/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import Foundation
+
+struct Representative: Codable {
+    
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+    
+}
